7cfc0c2c2219e9f5c757dfccf38a04acae914d89
Khavol <olehkhavar@gmail.com>
2024-02-29 16:12:25 +0200

    Add pages and routing

---
+++ src/app/pages/information/strategic-goals/strategic-goals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strategic-goals',
+  templateUrl: './strategic-goals.component.html',
+  styleUrls: ['./strategic-goals.component.scss']
+})
+export class StrategicGoalsComponent {
+
+}
---
+++ src/app/pages/information/values/values.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-values',
+  templateUrl: './values.component.html',
+  styleUrls: ['./values.component.scss']
+})
+export class ValuesComponent {
+
+}
